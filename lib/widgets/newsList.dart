78d65708c27c2dd4abb4ca8496fd5301c304f285
Mohammad Azam <azamsharp@gmail.com>
2019-12-04 13:20:58 -0600

    fixed the tap gesture for the newslist

--- lib/widgets/newsList.dart
+++ lib/widgets/newsList.dart
@@ -15,7 +15,7 @@ class NewsList extends StatelessWidget {
   double _getHeightForExpandedState(BuildContext context) {
     switch (this.expandedState) {
       case ExpandedState.compact:
-        return MediaQuery.of(context).size.height / 8;
+        return MediaQuery.of(context).size.height - 100;
       case ExpandedState.expanded:
         return MediaQuery.of(context).size.height - 60;
     }
@@ -28,58 +28,56 @@ class NewsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-    decoration: BoxDecoration(
-      color: Colors.grey[900], 
-      borderRadius: BorderRadius.circular(16)
-    ),
-    padding: EdgeInsets.all(20),
-    width: MediaQuery.of(context).size.width,
-    height: _getHeightForExpandedState(context),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        GestureDetector(
-              onTap: this.onHeaderTapped,
-              child: Container(
-              width: MediaQuery.of(context).size.width,
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text("Business News",
+    return GestureDetector(
+      onTap: this.onHeaderTapped,
+      child: Container(
+      decoration: BoxDecoration(
+        color: Colors.grey[900], 
+        borderRadius: BorderRadius.circular(16)
+      ),
+      padding: EdgeInsets.all(20),
+      width: MediaQuery.of(context).size.width,
+      height: _getHeightForExpandedState(context),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text("Business News",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold)),
+                Text("From Apple News",
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold))
+              ]),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 20,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  contentPadding: EdgeInsets.all(10),
+                  title: Wrap(children: <Widget>[
+                    Text("THE WALL STREET JOURNAL",
+                        style: TextStyle(color: Colors.white)),
+                    Text(
+                        "Houston weather drops to the record min 20F",
                         style: TextStyle(
                             color: Colors.white,
-                            fontSize: 32,
-                            fontWeight: FontWeight.bold)),
-                    Text("From Apple News",
-                        style: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 18,
+                            fontSize: 25,
                             fontWeight: FontWeight.bold))
-                  ])),
-        ),
-        Expanded(
-          child: ListView.builder(
-            itemCount: 20,
-            itemBuilder: (context, index) {
-              return ListTile(
-                contentPadding: EdgeInsets.all(10),
-                title: Wrap(children: <Widget>[
-                  Text("THE WALL STREET JOURNAL",
-                      style: TextStyle(color: Colors.white)),
-                  Text(
-                      "Houston weather drops to the record min 20F",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold))
-                ]),
-                trailing: Image.asset(_getRandomImage()),
-              );
-            },
-          ),
-        )
-      ],
-    ));
+                  ]),
+                  trailing: Image.asset(_getRandomImage()),
+                );
+              },
+            ),
+          )
+        ],
+      )),
+    );
   }
 }
